f92240950d9f3c69fbef61d8fa7c8d3827f7a69f
G59Hook <zach.suggs425@outlook.com>
2026-09-09 15:10:38 -0500

    Add Ruinborne KubeJS entry point

---
+++ kubejs/server_scripts/main.js
@@ -0,0 +1,3 @@
+// Ruinborne server scripts entry point.
+// Progression recipe work will be added in later test passes after exact item IDs are verified.
+console.info('[Ruinborne] Server scripts loaded');
